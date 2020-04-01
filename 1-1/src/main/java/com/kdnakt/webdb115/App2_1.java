22b701af5e467d7fe56f16f00f2b3bceec960304
kdnakt <a.kid.1985@gmail.com>
2020-04-01 20:09:48 +0900

    add 2-1

--- 1-1/src/main/java/com/kdnakt/webdb115/App2_1.java
+++ 1-1/src/main/java/com/kdnakt/webdb115/App2_1.java
@@ -4,17 +4,15 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Scanner;
 
-public class App {
+public class App2_1 {
     public static void main(String[] args) throws FileNotFoundException {
-        InputStream in = App.class.getClassLoader().getResourceAsStream("input");
+        InputStream in = App2_1.class.getClassLoader().getResourceAsStream("input2_1");
         Scanner cin = new Scanner(in);
         long start = System.currentTimeMillis();
-        int N = cin.nextInt();
-        for (int i = 0; i < N; i++) {
-            int A = cin.nextInt();
-            long ans = (long) A * (A + 1) / 2;
-            System.out.println(ans);
-        }
+        int A = cin.nextInt(), B = cin.nextInt();
+        int ans = B / A;
+        if (B % A != 0) ans++;
+        System.out.println(ans);
         System.out.println(System.currentTimeMillis() - start + " ms");
         cin.close();
     }
