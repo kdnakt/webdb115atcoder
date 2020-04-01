fe0d8c1eb8200e14bf17ebc5ee3a840483468e86
kdnakt <a.kid.1985@gmail.com>
2020-04-02 08:21:27 +0900

    2-2 use sort

--- 1-1/src/main/java/com/kdnakt/webdb115/App2_2.java
+++ 1-1/src/main/java/com/kdnakt/webdb115/App2_2.java
@@ -2,6 +2,7 @@ package com.kdnakt.webdb115;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class App2_2 {
@@ -12,13 +13,21 @@ public class App2_2 {
         int N = cin.nextInt();
         int[] A = new int[N];
         for (int i = 0; i < N; i++) A[i] = cin.nextInt();
-        int[] B = new int[N + 1];
-        for (int i = 0; i < N; i++) B[A[i]]++;
+        // int[] B = new int[N + 1];
+        // for (int i = 0; i < N; i++) B[A[i]]++;
+        // int Same = -1, No = -1;
+        // for (int i = 1; i <= N; i++) {
+        //     if (B[i] == 0) No = i;
+        //     if (B[i] == 2) Same = i;
+        // }
         int Same = -1, No = -1;
-        for (int i = 1; i <= N; i++) {
-            if (B[i] == 0) No = i;
-            if (B[i] == 2) Same = i;
+        Arrays.sort(A);
+        if (A[0] != 0) No = 0;
+        for (int i = 1; i < N; i++) {
+            if (A[i - 1] == A[i]) Same = A[i];
+            if (A[i - 1] == A[i] - 2) No = A[i] - 1;
         }
+        if (A[N-1] != N) No = N;
         if (Same == -1) System.out.println("CORRECT");
         else System.out.println(Same + " " + No);
         System.out.println(System.currentTimeMillis() - start + " ms");
