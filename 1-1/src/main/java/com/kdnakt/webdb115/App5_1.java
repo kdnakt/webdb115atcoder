12e452b8c322ced6dacfc5575d714be354f2c974
kdnakt <a.kid.1985@gmail.com>
2020-04-13 02:27:29 +0900

    5-1: wrong answer with dfs

---
+++ 1-1/src/main/java/com/kdnakt/webdb115/App5_1.java
@@ -0,0 +1,38 @@
+package com.kdnakt.webdb115;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class App5_1 {
+
+    static int N;
+    static int[] H;
+    public static void main(String[] args) throws FileNotFoundException {
+        try (
+            InputStream in = App5_1.class.getClassLoader().getResourceAsStream("input5_1");
+            Scanner cin = new Scanner(in);
+        ) {
+            long start = System.currentTimeMillis();
+            N = cin.nextInt();
+            H = new int[N+1];
+            for (int i = 0; i < N; i++) {
+                H[i] = cin.nextInt();
+            }
+            System.out.println(dfs(0));
+            System.out.println(System.currentTimeMillis() - start + " ms");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static int dfs(int pos) {
+        if (pos >= N) return 99999999;
+        if (pos == N-1) return 0;
+        int step1 = dfs(pos+1) + Math.abs(H[pos+1]-H[pos]);
+        int step2 = dfs(pos+2) + Math.abs(H[pos+2]-H[pos]);
+        return Math.min(step1, step2);
+    }
+
+}
